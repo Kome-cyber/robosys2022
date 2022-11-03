ab4dea9fad7515ba48cfcc3a2a201a955e462087
Koumei Ainoya <s21c1003ak@s.chibakoudai.jp>
2022-11-03 12:11:01 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -12,11 +12,11 @@ out=$(seq 5 | ./plus)
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
-[ "${out}" = " " ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
-[ "${out}" = " " ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 
